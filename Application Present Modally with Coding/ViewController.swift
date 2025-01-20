6a43741fb8c3a4b871d2e5af2e7b04c28c418a38
zemfirazeynal <zemfirazeynal@gmail.com>
2025-01-20 06:25:35 -0800

    Initial Commit

---
+++ Application Present Modally with Coding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Application Present Modally with Coding
+//
+//  Created by Zemfira Asadzade on 20.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
